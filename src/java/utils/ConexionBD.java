6004be054960828dc04378abdc11a51c7351b97b
alejaSj <alejandrosj015@gmail.com>
2025-05-14 20:46:19 -0600

    toques a ConexionBD

--- src/java/utils/ConexionBD.java
+++ src/java/utils/ConexionBD.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBD {
-    private static final String URL = "jdbc:mysql://localhost:3306/nuevaTabla?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/AgenciaViajes?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "soyadmin";
 
     static {
         try {
